43d41ab13c85367899a84920ba810ace3b0879e6
Michael Truong <michael.d.truong@icloud.com>
2016-08-18 16:19:20 -0500

    initial commit

---
+++ public/portfolio.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>My Portfolio</title>
+		<link rel="stylesheet" href="/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/portfolio.css">
+	</head>
+	<body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="michael">MICHAEL TRUONG <span class="nameline">|</span> DESIGN</div>
+<!--                 <div class="mail"><img src="/img/mail.png" class="mailsymbol"></div> -->
+                <div class="bannerleft"></div>
+                <div class="bannerright"></div>
+                <div class="verttitle">PORTFOLIO</div>
+                <div class="portfiller"></div>
+                <div class="portborder1"></div>
+                <div class="portfront">
+
+                    <div class="slidecontainer1">
+                        <img src="/img/craft.jpg" class="slide" alt="image" />
+                        <img src="/img/finalize.jpg" class="slide" alt="image" />
+                        <img src="/img/maintain.jpg" class="slide" alt="image" />
+                        <img src="/img/imagin.jpg" class="slide" alt="image" />
+                    </div>
+
+                    <div class="slidecontainer2">
+                        <img src="/img/timeless.jpg" class="slide2" alt="image" />
+                        <img src="/img/elegant.jpg" class="slide2" alt="image" />
+                        <img src="/img/creative.jpg" class="slide2" alt="image" />
+                        <img src="/img/beautiful.jpg" class="slide2" alt="image" />
+                    </div>
+
+                    <div class="slidecontainer3">
+                        <img src="/img/aesthetics.jpg" class="slide3" alt="image" />
+                        <img src="/img/function.jpg" class="slide3" alt="image" />
+                        <img src="/img/experience.jpg" class="slide3" alt="image" />
+                        <img src="/img/design.jpg" class="slide3" alt="image" />
+                    </div>
+
+                </div>
+                <div class="bannerfull"></div>
+                <div class="navb">
+
+                    <div class="butn"><a id="homebtn" onclick="gohome()">Home</a></div>
+                    <div class="butn"><a id="aboutbtn" onclick="goabout()">About</a></div>
+                    <div class="butn"><a id="contactbtn" onclick="gocontact()">Contact</a></div>
+
+                </div>
+                <div class="circle"><img src="/img/techcircle.png"></div>
+                <div class="infoborder1"></div>
+                <div class="infoborder2"></div>
+                <div class="infoborder3"></div>
+                <div class="infoborder4"></div>
+
+
+                <div class="info" id="home">
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit</p>
+                </div>
+
+                <div class="info" id="about">
+
+                    <img src="/img/placeholder.png" class="portimage aboutimage">
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu</p>
+                </div>
+
+                <div class="info" id="contact">
+
+                    <form method="POST">
+                        <input type="text" id="formname" name="name" class="formtext" placeholder="Your Name*">
+                        <input type="text" id="formnumber" name="number" class="formtext" placeholder="Your Contact Number">
+                        <br>
+                        <input type="text" id="formemail" name="email" class="formtext" placeholder="Your Email*">
+                        <textarea type="text" id="formmessage" name="message" class="formtextarea" placeholder="Your Message*"></textarea>
+                        <button type="submit" id="formsubmit">SEND</button>
+                    </form>
+
+                </div>
+
+                <div class="hortitle">PORTFOLIO</div>
+                
+                <div class="portfolio">
+
+
+                    <div class="portodd">
+                    <img src="/img/placeholder.png" class="portimage">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+
+                    <div class="porteven">
+                    <img src="/img/placeholder.png" class="portimage">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+
+                    <div class="portodd">
+                    <img src="/img/placeholder.png" class="portimage">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+
+                    <div class="porteven">
+                    <img src="/img/placeholder.png" class="portimage">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+
+                    <div class="portodd">
+                    <img src="/img/placeholder.png" class="portimage">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+
+                    <div class="porteven">
+                    <img src="/img/placeholder.png" class="portimage">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+
+                </div>
+
+                <div class="portborder2"></div>
+                <div class="footerleft"></div>
+                <div class="footerright"></div>
+                <div class="footerfull"></div>
+
+            </div>
+        </div>
+
+        <script 
+            src="//code.jquery.com/jquery-1.11.3.min.js">
+        </script>
+        <script 
+            src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">
+        </script>
+        <script>
+            // function gohome() {
+            //     document.getElementById("about").style.display= "none";
+            //     document.getElementById("contact").style.display= "none";
+            //     document.getElementById("home").style.display= "block";
+            // }
+            // function goabout() {
+            //     document.getElementById("home").style.display= "none";
+            //     document.getElementById("contact").style.display= "none";
+            //     document.getElementById("about").style.display= "block";
+            // }
+            // function gocontact() {
+            //     document.getElementById("about").style.display= "none";
+            //     document.getElementById("home").style.display= "none";
+            //     document.getElementById("contact").style.display= "block";
+            // }
+
+            function goabout() {
+                var about = document.getElementById("about");
+                var fade = 0;
+                var id = setInterval(frame, 5); 
+                document.getElementById("home").style.display= "none";
+                document.getElementById("contact").style.display= "none";
+                document.getElementById("home").style.opacity= 0;
+                document.getElementById("contact").style.opacity= 0;
+                about.style.display = "block";
+                function frame() {
+                    if (fade > 1) {
+                        clearInterval(id);
+                    } else {
+                        fade = fade + 0.01;
+                        about.style.opacity = fade;
+                    }
+                }
+            }
+
+            function gocontact() {
+                var contact = document.getElementById("contact");
+                var fade = 0;
+                var id = setInterval(frame, 5);
+                document.getElementById("home").style.display= "none";
+                document.getElementById("about").style.display= "none";
+                document.getElementById("home").style.opacity= 0;
+                document.getElementById("about").style.opacity= 0;
+                contact.style.display = "block";
+                function frame() {
+                    if (fade > 1) {
+                        clearInterval(id);
+                    } else {
+                        fade = fade + 0.01;
+                         contact.style.opacity = fade;
+                    }
+                }
+            }
+
+            function gohome() {
+                var home = document.getElementById("home");
+                var fade = 0;
+                var id = setInterval(frame, 5);
+                document.getElementById("about").style.display= "none";
+                document.getElementById("contact").style.display= "none";
+                document.getElementById("about").style.opacity= 0;
+                document.getElementById("contact").style.opacity= 0;
+                home.style.display = "block";
+                function frame() {
+                    if (fade > 1) {
+                        clearInterval(id);
+                    } else {
+                        fade = fade + 0.01;
+                         home.style.opacity = fade;
+                    }
+                }
+            }
+            
+        </script>
+	</body>
+</html>
